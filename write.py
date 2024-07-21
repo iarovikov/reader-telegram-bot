5e691803c9b63eb7393fbe22acc89a426e6c4ddb
Vadim Iarovikov <vadim.yarovikov@gmail.com>
2024-07-21 11:59:30 +0200

    add bot

--- write.py
+++ write.py
@@ -1,19 +1,42 @@
 import os
-from dotenv import load_dotenv, dotenv_values 
-import datetime
-import requests 
-import json
+from dotenv import load_dotenv 
+import logging
+from telegram import Update
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
+
 
 load_dotenv()
-token = os.getenv('READWISE_TOKEN')  # or however you're storing your token
+readwise_token = os.getenv('READWISE_TOKEN')  # or however you're storing your token
+telegram_token = os.getenv('TELEGRAM_TOKEN')
 
-response = requests.post(
-        url="https://readwise.io/api/v3/save/",
-        headers={"Authorization": f"Token {token}"}, verify=False,
-        json={
-            "url": "https://yourapp.com#document1",
-            "html": "<p>Your document content here</p>",
-        }
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
 
-print(response.json())
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(telegram_token).build()
+    
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    
+    application.run_polling()
+#response = requests.post(
+#        url="https://readwise.io/api/v3/save/",
+#        headers={"Authorization": f"Token {readwise_token}"}, verify=False,
+#        json={
+#            "url": "https://yourapp.com#document1",
+#            "html": "<p>Your document content here</p>",
+#        }
+#)
+#
